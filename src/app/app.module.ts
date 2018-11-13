c69c1b60cf4bb6267800998f385d25c73d2ef47b
Josh Turgeon <jturgeon88@gmail.com>
2018-11-13 10:26:00 -0600

    register customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
-import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomersComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CustomersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
